81fe91d9ed9235305bece479cf3a186b07a12f81
hyuksun98 <gurtjs0116@gmail.com>
2024-10-12 20:28:52 +0900

    update blog name and email address

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "hyuksun98",
     image: "/hyuksun98-avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Passionate Developer",
+    bio: "Maximum Effort",
+    email: "gurtjs0116@gmail.com",
+    // linkedin: "morethanmin",
+    github: "hyuksun98",
     instagram: "",
   },
   projects: [
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Hyuksun-log",
+    description: "welcome to Hyuksun-log!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
